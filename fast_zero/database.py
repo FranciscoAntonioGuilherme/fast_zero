db2b9ce9f52a8d7b6dc9e86155093bb0601d4a74
Francisco <francisco.agn@hotmail.com>
2024-01-22 15:33:37 -0300

    Atualizando endpoints para usar o banco de dados real.

---
+++ fast_zero/database.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+from fast_zero.settings import Setting
+
+
+engine = create_engine(Setting().DATABASE_URL)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
